9b3f62f76e1096c5a8c77511a585182b2b99d10a
Patrick Day <patrick.day@weather.com>
2015-04-27 13:55:24 -0400

    Updated Drupal test

--- Drupal/src/main/java/com.weather.automation.drupal/HomePage.java
+++ Drupal/src/main/java/com.weather.automation.drupal/HomePage.java
@@ -1,8 +1,18 @@
 package com.weather.automation.drupal;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+
 public class HomePage {
+    private static WebElement menu = Utility.verifyObject("admin-menu");
+    //private WebElement content = menu.findElement(By.partialLinkText("/admin/content"));
 
     public static boolean isOpen(){
         return true;
+    }
+
+    public static void nagivateTo(){
+        Utility.verifyObjectByXpath("//*[@id=\"admin-menu-menu\"]/li[1]/a").click();
     }
 }
--- Drupal/src/test/java/com.weather.automation.drupal/DrupalTests.java
+++ Drupal/src/test/java/com.weather.automation.drupal/DrupalTests.java
@@ -1,29 +1,30 @@
 package com.weather.automation.drupal;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import static org.testng.Assert.assertTrue;
 
 public class DrupalTests {
     WebDriver driver;
 
-    @BeforeTest
+    @BeforeClass
     public void setUp(){
         driver = Driver.getDriver();
-    }
-
-    @AfterTest
-    public void tearDown(){
-        driver.close();
-    }
-
-    @Test
-    public void performFlightSearch(){
         driver.get("http://adev.weather.com/user");
         LoginPage.user("mohammed.islam").withPassword("DEV").login();
         assertTrue(HomePage.isOpen());
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void addContent(){
+        assertTrue(HomePage.isOpen());
+        HomePage.nagivateTo();
+
     }
 }
